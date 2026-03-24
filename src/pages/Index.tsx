0c345e01a8c511f00064bd02fa6c38147361571e
Skip <skip@usecurling.com>
2026-03-24 15:09:33 +0000

    Fix reference error on Index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import { Badge } from '@/components/ui/badge'
 import useAppStore from '@/stores/useAppStore'
+import { cn } from '@/lib/utils'
 
 const ANNOUNCEMENTS = [
   {
